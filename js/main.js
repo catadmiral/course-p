20428b5d813d5fb0987527872ed6e766e86cd265
catAdmiral <catmilana9@gmail.com>
2020-10-20 03:46:18 +0300

    Комментарий

---
+++ js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Урок 7 ----------------
+
+// alert('Привет!!!!');
+// const result = confirm("Ты тут???");
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18 лет?', '18');
+// console.log(answer + 10);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+
+// Урок 7 Интерполяция ----------------
+
+// const category = 'toys';
+// console.log('https:/someurl.com/' + category);
+// результат: сложение строк https:/someurl.com/toys
+
+const category = 'toys';
+console.log(`https:/someurl.com/${category}`);
+// результат: сложение строк https:/someurl.com/toys
